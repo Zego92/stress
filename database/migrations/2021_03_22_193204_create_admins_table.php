89abe8fc8f8705c5a4552a4c004a3ad06d126381
Genesis8448 <Ax0245aa>
2021-05-05 18:37:15 +0300

    delete PhpDoc

--- database/migrations/2021_03_22_193204_create_admins_table.php
+++ database/migrations/2021_03_22_193204_create_admins_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAdminsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
@@ -27,11 +22,6 @@ class CreateAdminsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('admins');
