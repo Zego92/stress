41ceb2240e271dbe1f06001e7831b5ff3ab05fe9
evgeniytimchenco <Timchenco92@gmail.com>
2021-05-24 15:26:35 +0300

    prepare FormRequests to precessing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
-        console.log(toastr)
         toastr.options.closeButton = true;
         @if(session('success'))
         toastr.success( "{{session()->get('success')}}", {timeOut: 2000})
